b154fa3b3354692a423f73ebf78ed6abaabebb74
Markoz Pena <markdrew53@gmail.com>
2021-06-14 15:45:26 -0500

    build: Module augmentation for Session and JWT types

---
+++ @types/next-auth.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    accessToken?: string | unknown;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    accessToken?: string | unknown;
+  }
+}

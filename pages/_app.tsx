f787cc348012c6ab54f5c8c53715eba0fde616d9
Markoz Pena <markdrew53@gmail.com>
2021-06-14 16:11:36 -0500

    feat: Wrap the Next.js application in the `<Provider>` component of next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import { AppProps } from 'next/app';
+import { Provider } from 'next-auth/client';
 
 import '@styles/main.css';
 
 const MyApp = ({ pageProps, Component }: AppProps) => (
-  <Component {...pageProps} />
+  <Provider session={pageProps.session}>
+    <Component {...pageProps} />
+  </Provider>
 );
 
 export default MyApp;
